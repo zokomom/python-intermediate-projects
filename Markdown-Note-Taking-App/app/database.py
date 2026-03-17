dd127c5feac7c07ea7cb0491697555e1126c46e7
Atharv <172103387+zokomom@users.noreply.github.com>
2026-03-17 22:54:46 +0530

    feat:introduced database using sqlalchemy & config for fetching env variable from .env and validating it

---
+++ Markdown-Note-Taking-App/app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+DATABASE_URL = settings.database_url
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+
+class Base(declarative_base):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
