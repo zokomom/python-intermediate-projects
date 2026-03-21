f12363ffd0b33a7d75b829274854acb1ac7a867a
Atharv <172103387+zokomom@users.noreply.github.com>
2026-03-21 20:33:22 +0530

    feat:updated pydantic schemas and added new schema for grammar check and createNote

--- Markdown-Note-Taking-App/app/pydantic_schemas.py
+++ Markdown-Note-Taking-App/app/pydantic_schemas.py
@@ -1,9 +1,19 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class GetNotes(BaseModel):
     id:int
     title:str
     markdown_content:str
+    created_at:datetime
     model_config={
         "from_attributes":True
     }
+
+class CreateNote(BaseModel):
+    title:str
+    markdown_content:str
+
+class GrammarCheck(BaseModel):
+    title:str
+    markdown_content:str
