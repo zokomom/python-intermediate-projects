2340d735cd417514fc407ff3910cd4de6768e900
Atharv <172103387+zokomom@users.noreply.github.com>
2026-03-21 20:32:12 +0530

    feat:added new column to the note model

--- Markdown-Note-Taking-App/app/models.py
+++ Markdown-Note-Taking-App/app/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 from .database import Base
 
 
@@ -7,3 +9,4 @@ class Note(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     markdown_content = Column(Text, nullable=False)
+    created_at=Column(TIMESTAMP(timezone=True),server_default=text("now()"))
